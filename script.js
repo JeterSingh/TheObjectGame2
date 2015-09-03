b9340b3d388318d6ef7bd1733f9d05e0b81e65f7
Ranjit Ahluwalia <ranjit.ahluwalia@gmail.com>
2015-09-02 21:09:43 -0600

    Object 2 exercise

--- script.js
+++ script.js
@@ -11,19 +11,29 @@ var startingHits= 0;
 var playerName= "PlayerName";
 
 // Item Constructor HERE
-
+var ReduceDamage = function(name, modifier, description){
+    this.name = name;
+    this.modifier = modifier;
+    this.description = description;
+    this.draw = function(){
+       return '<div class="item">'+ this.name +'</div>'; //added to body upshift challenge
+    }
+}
 
 
 
 //global items placeholder HERE
 var items = {
-  
+  shield: new ReduceDamage("Shield",1,"This is an awesome shield!"),
+  forcefield: new ReduceDamage("Forcefield",2,"This is an awesome forcefield!"),
+  deflector: new ReduceDamage("Deflector",3,"This is an awesome deflector!")
 };
 
 var player = {
   name: "PlayerName",
   health: startingHealth,
   hits: startingHits,
+  items:[items.shield, items.forcefield, items.deflector],  //we added this text
   
   slap: function() {
     this.hit(1);
@@ -34,8 +44,16 @@ var player = {
   kick: function() {
     this.hit(10);
   },
+  addMods: function() {
+    var total = 0;
+    for (var i = 0; i < player.items.length; i++) {
+      total += player.items[i].modifier;
+    }
+    return total;
+  },
+  
   hit: function(damage) {
-    this.health -= damage;
+    this.health = this.health - damage + this.addMods();
     this.hits++;
     if (this.health <= 0) {
       this.health = 0;
@@ -46,7 +64,8 @@ var player = {
       this.health = startingHealth,
       this.hits = startingHits,
     update();
-  },
+  }
+  
 }
 
 function update() {
@@ -55,8 +74,18 @@ function update() {
   document.getElementById("hits").innerText = player.hits;
   
   //IF HEALTH STATEMENT
-
-  
+  if(player.health <=0) {
+    document.getElementById("player-panel").classList.add("panel-danger")
+  } else{
+    document.getElementById("player-panel").classList.remove("panel-danger")
+  } 
+  drawItems();
 }
-
+function drawItems() {
+  var toDraw = "";
+  for (var i = 0; i < player.items.length; i++) {
+    toDraw += player.items[i].draw()
+  }
+  document.getElementById("player-items").innerHTML = toDraw;
+}
 update();
